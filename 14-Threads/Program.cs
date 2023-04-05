7f05525894b461af11d64dc028dd473e250c41ac
Ali Hassan <engineralihassan@gmail.com>
2023-04-05 01:52:34 -0700

    code till threads

---
+++ 14-Threads/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _14_Threads
+{
+   public class Program
+    {
+        public static void func1()
+        {
+            for (int i = 1; i <50; i++)
+            {
+                Console.WriteLine("Func1 calls"+i);
+            }
+        }
+        //
+        public static void func2()
+        {
+            for (int i = 1; i < 50; i++)
+            {
+                Console.WriteLine("Func12 calls" + i);
+                if(i == 25)
+                {
+                    Console.WriteLine("thread is on sleep");
+                    Thread.Sleep(8000);
+                }
+            }
+        }
+        //
+        public static void func3()
+        {
+            for (int i = 1; i < 50; i++)
+            {
+                Console.WriteLine("Func3 calls" + i);
+            }
+        }
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Multi threading in c#");
+            /*
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+
+            Console.WriteLine("Current executing Thread is {0}",t.Name);
+            */
+            Thread t1 = new Thread(func1);
+            Thread t2 = new Thread(func2);
+            Thread t3 = new Thread(func3);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            //Program.func1();
+            //Program.func2(); 
+            //Program.func3();
+
+        }
+    }
+}
---
+++ 14String-and-StringBuilders/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace _14String_and_StringBuilders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String and String Builders");
+            /*
+            // Normal string 
+            string s = "Ali";
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                s=s+i;
+            }
+            sw.Stop();
+            */
+            // strings are the value based and strings are immutable
+            // string builder
+            /*
+            StringBuilder sb = new StringBuilder("Hello");
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            for (int i=0; i<100000; i++)
+            {
+                sb.Append(i);
+            }
+            sw1.Stop();
+            Console.WriteLine("Time taken by string{0}",sw.ElapsedMilliseconds);
+            Console.WriteLine("Time taken by stringBuilder{0}", sw1.ElapsedMilliseconds);
+            */
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.Append("Ali");
+            sb.AppendLine("Hassan");
+            sb.Append("Akram");
+            sb.AppendFormat("{0:C}", 23);
+            sb.AppendFormat("{0:N}", 23);
+            sb.AppendFormat("{0:X}", 23);
+            Console.WriteLine(sb);
+            sb.Insert(4, "MR");
+           sb.Remove(4, 5);
+            sb.Replace("Hello", "Hy");
+            Console.WriteLine(sb);
+
+        }
+    }
+}
