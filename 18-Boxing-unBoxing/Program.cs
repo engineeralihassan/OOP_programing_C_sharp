2584f42621d0890b7d5bd176279b8310b526fc36
Ali Hassan <engineralihassan@gmail.com>
2023-04-06 00:14:24 -0700

    All The code untill the Boxing and unboxing in c#

---
+++ 18-Boxing-unBoxing/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _18_Boxing_unBoxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boxing and unBoxing In C#");
+            // Boxing means implicitly type convertions
+            int num1 = 21;
+            object obj = num1;
+            Console.WriteLine(obj);
+            // int num2=obj this is error
+            int num2 =(int) obj;
+        }
+    }
+}
