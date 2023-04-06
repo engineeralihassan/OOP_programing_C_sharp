7db35afb0da54aa693c0e2f34a74691595f4395e
Ali Hassan <engineralihassan@gmail.com>
2023-04-06 00:30:35 -0700

    All Code Till Guid

---
+++ 19-Guide/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _19_Guide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Globally unique identifier");
+            Guid g = Guid.NewGuid();
+            //Console.WriteLine(g.ToString().
+            //    Replace("-", string.Empty));
+           // Console.WriteLine(g.ToString("N"));
+            Console.WriteLine(g.ToString("N").Substring(0,15) );
+            Console.WriteLine(g);
+
+        }
+    }
+}
